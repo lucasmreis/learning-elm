535786c5b56eb061d928a1423fba32500fd65767
Lucas Reis <lucasmreis@gmail.com>
2016-11-16 22:50:13 -0200

    all tests ok

--- part-4/tests/Tests.elm
+++ part-4/tests/Tests.elm
@@ -8,17 +8,29 @@ import Cards exposing (..)
 
 all : Test
 all =
-    fuzz (intRange -20 20) "parseNumValue"
-        <| \number ->
-            let
-                parsed =
-                    parseNumValue (toString number)
-            in
-                case parsed of
-                    Just (Num v) ->
-                        Expect.true "Number should be >= 2 and <= 10 when Just Num v"
-                            (v >= 2 && v <= 10)
+    describe "parseNumValue"
+        [ test "cannot be less than 2"
+            <| \() -> Expect.equal (parseNumValue "1") Nothing
+        , test "minimum of 2"
+            <| \() -> Expect.equal (parseNumValue "2") (Just (Num 2))
+        , test "maximum of 10"
+            <| \() -> Expect.equal (parseNumValue "10") (Just (Num 10))
+        , test "cannot be more than 10"
+            <| \() -> Expect.equal (parseNumValue "11") Nothing
+        , fuzz int "parseNumValue"
+            <| \number ->
+                let
+                    parsed =
+                        number
+                            |> toString
+                            |> parseNumValue
+                in
+                    case parsed of
+                        Just (Num v) ->
+                            Expect.true "Number should be >= 2 and <= 10 when Just Num v"
+                                (v >= 2 && v <= 10)
 
-                    _ ->
-                        Expect.false "Number should not be >= 2 and <= 10 when Nothing"
-                            (number >= 2 && number <= 10)
+                        _ ->
+                            Expect.false "Number should not be >= 2 and <= 10 when Nothing"
+                                (number >= 2 && number <= 10)
+        ]
